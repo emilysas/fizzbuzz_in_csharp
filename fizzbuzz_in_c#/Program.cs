cc7cf45b7858d3c4526a28ad4cb3299d282d93e3
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-21 13:46:57 +0000

    working fizzbuzz using if/else statements

---
+++ fizzbuzz_in_c#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string input = Console.ReadLine ();
+			int num = int.Parse (input);
+			if(num % 15 == 0)
+			{
+				Console.WriteLine ("Fizzbuzz");
+		    }
+			else if(num % 3 == 0)
+			{
+				Console.WriteLine ("Fizz");
+		    }
+			else if(num % 5 == 0)
+			{
+				Console.WriteLine ("Buzz");
+			}
+			else
+			{
+				Console.WriteLine (num);
+			};
+		}
+	}
+}
